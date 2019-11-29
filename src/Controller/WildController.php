f2504d150e5a73004cb1cfc94df0a49763eb23cd
batryad <ryadus2001@gmail.com>
2019-11-29 15:06:24 +0100

    pasmal

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -10,12 +10,22 @@ use Symfony\Component\Routing\Annotation\Route;
 Class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild_index")
+     * @Route("/wild/show/{slug}",
+     *        name="wild_show",
+     *        defaults={"slug"="Aucune série sélectionnée, veuillez choisir une série"},
+     *        requirements={"slug": "[a-z0-9-]+"})
+     * @param $slug
+     * @return Response
      */
-    public function index() :Response
+    public function show($slug) :Response
     {
-        return $this->render('wild/index.html.twig', [
+
+      $slug = str_replace('-', ' ', $slug);
+      $slug = ucwords($slug);
+        return $this->render('wild/show.html.twig', [
             'website' => 'Wild Séries',
+            'slug' => $slug,
+            'name' => 'Titre de film'
         ]);
     }
 }
